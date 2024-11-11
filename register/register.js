bbf73e30e8ef355a2e6c793e1d8c158b5c7934db
joseph-robles-jr <rjosepha@byui.edu>
2024-11-11 15:02:03 -0700

    completed register.

--- register/register.js
+++ register/register.js
@@ -1,41 +1,73 @@
-function participantTemplate(count){ //count is the current number of participants shown
-    `
-              <section class="participant${count}">
-            <p>Participant 1</p>
-            <div class="item">
-              <label for="fname"> First Name<span>*</span></label>
-              <input id="fname${count}" type="text" name="fname" value="" required />
-            </div>
-            <div class="item activities">
-              <label for="activity">Activity #<span>*</span></label>
-              <input id="activity${count}" type="text" name="activity" />
-            </div>
-            <div class="item">
-              <label for="fee">Fee ($)<span>*</span></label>
-              <input id="fee${count}" type="number" name="fee" />
-            </div>
-            <div class="item">
-              <label for="date">Desired Date <span>*</span></label>
-              <input id="date${count}" type="date" name="date" />
-            </div>
-            <div class="item">
-              <p>Grade</p>
-              <select>
-                <option selected value="" disabled selected></option>
-                <option value="1">1st</option>
-                <option value="2">2nd</option>
-                <option value="3">3rd</option>
-                <option value="4">4th</option>
-                <option value="5">5th</option>
-                <option value="6">6th</option>
-                <option value="7">7th</option>
-                <option value="8">8th</option>
-                <option value="9">9th</option>
-                <option value="10">10th</option>
-                <option value="11">11th</option>
-                <option value="12">12th</option>
-              </select>
-            </div>
-          </section>
-    `;
+import participantTemplate from "./template.mjs";
+
+
+var i = 2; //counter
+
+
+function addparticipant(){
+  var i_work = participantTemplate(i++);
+  button_event.insertAdjacentHTML("beforebegin", i_work);
+
 }
+
+
+var button_event = document.querySelector("#add");
+button_event.addEventListener("click", addparticipant);
+
+var submit_button = document.querySelector("form");
+submit_button.addEventListener("submit", submitForm);// when click is submited, submit form
+
+
+
+function submitForm(event) {
+  event.preventDefault();
+  //total fees
+  var returned_total_fees = totalFees();
+  console.log(returned_total_fees);
+
+
+  function return_recipt(){ // Create a function like participantTemplate above, where we return a string representation of a receipt page
+    // Just the total number of participants with the summed fee amount
+    let reciptPage = `There are ${i-1} participants. Total: $${returned_total_fees}`; 
+     // display reciptPage in html
+
+     document.getElementById('summary').innerHTML = reciptPage;
+     return reciptPage;
+  }
+  document.querySelector('form').style.display = 'none';
+
+  return_recipt();
+  // Hide the form with form.style.display = 'none'
+
+
+  }
+
+
+function totalFees() {
+  // the selector below lets us grab any element that has an id that begins with "fee"
+  let feeElements = document.querySelectorAll("[id^=fee]");
+  console.log(feeElements);
+  // querySelectorAll returns a NodeList. It's like an Array, but not exactly the same.
+  // The line below is an easy way to convert something that is list-like to an actual Array so we can use all of the helpful Array methods...like reduce
+  // The "..." is called the spread operator. It "spreads" apart the list, then the [] we wrapped it in inserts those list items into a new Array.
+  feeElements = [...feeElements];
+  
+  let sum = 0;
+  for (let item of feeElements)
+  {
+   sum+= parseFloat(item.value);
+  
+  }
+  return sum;
+  
+  // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
+  // Remember that the text that was entered into the input element will be found in the .value of the element.
+  
+  // once you have your total make sure to return it!
+  
+  }
+
+
+
+
+
