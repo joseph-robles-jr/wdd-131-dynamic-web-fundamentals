71571259e1a122149e82960e2a58b07c88a6a11f
joseph-robles-jr <rjosepha@byui.edu>
2024-11-16 23:29:53 -0700

    started recipie

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,3 +1,11 @@
-document.getElementById("menu-button").addEventListener("click", function() {
-  alert("Button was clicked!");
+document.addEventListener('DOMContentLoaded', function() { //DOMContentLoaded ensures that it runs after HTML is loaded. I am not sure why this is here but I was having issues and AI siggested it.
+  const menuButton = document.getElementById('menuButton'); //selects the menubutton element.
+  const navbar = document.getElementById('navbar'); // selects the nav bar
+
+  menuButton.addEventListener('click', function() {  
+    navbar.classList.toggle('hide'); //removes the .hide attribute to show the menu
+    console.log("Button pressed");
+  });
 });
+
+
